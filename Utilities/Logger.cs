e0af2ac4958d5621514fbfcef5621790c53b00e2
kanupindi14 <kavyashree.a14@gmail.com>
2025-03-21 04:37:14 +0000

    Added config file and Logger file

---
+++ Utilities/Logger.cs
@@ -0,0 +1,20 @@
+namespace ArgusRestaurentCheckoutAutomation.Utils
+{
+    public static class Logger
+    {
+        public static void Info(string message)
+        {
+            Console.WriteLine($"[INFO] {message}");
+        }
+
+        public static void Error(string message)
+        {
+            Console.WriteLine($"[ERROR] {message}");
+        }
+
+        public static void Debug(string message)
+        {
+            Console.WriteLine($"[DEBUG] {message}");
+        }
+    }
+}
